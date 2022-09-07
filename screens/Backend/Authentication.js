edc11ba3daf803b75b4b33ed5443dad41627974d
Membah <87417620+Membah@users.noreply.github.com>
2022-09-07 09:44:56 +0300

    Add files via upload

--- screens/Backend/Authentication.js
+++ screens/Backend/Authentication.js
@@ -1,1 +1,19 @@
+import { watchPositionAsync } from 'expo-location';
+import * as firebase from 'firebase';
+import { Alert } from 'react-native';
 
+export async function registration (name,email,password){
+   await firebase.auth().createUserWithPasswordAndEmail(email,password);
+   const currentUser = firebase.auth().currentUser;
+
+   const db = firebase.firestore();
+   db.collection('user').doc(currentUser.uid).set({
+    email: name,
+    password:password,
+   });
+    try {
+        
+    } catch (error) {
+      Alert.alert("There's something".error)  
+    }
+}
