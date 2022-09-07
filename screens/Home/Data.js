30a6ce4fdbd0c694e2bebec4d2e0b938769fffa9
Membah <87417620+Membah@users.noreply.github.com>
2022-09-07 09:44:24 +0300

    Add files via upload

--- screens/Home/Data.js
+++ screens/Home/Data.js
@@ -1,1 +1,27 @@
-
+import nail from '../../assets/nail.jpg'
+import Plumber from '../../assets/plumba.jpg'
+import doc from '../../assets/doc.jpg'
+const serviceData = [
+    {  
+        name:"Nails Services",
+        category:"Nail Specialist",
+        numberCompany:"55",
+        bgColor: "#c51162",
+        image: nail,
+    },
+    {
+        name:"Plumber Services",
+        category:"Plumber",
+        numberCompany:"35",
+        bgColor: "#3722f6",
+        image: Plumber,
+    },
+    {
+        name:"Best Treatment",
+        category:"Doctor",
+        numberCompany:"25",
+        bgColor: "#aa00ff",
+        image: doc ,
+    }
+]
+export default serviceData
---
+++ screens/Home/ServiceList.js
@@ -0,0 +1,31 @@
+
+import doctor from '../../assets/doctor.jpg'
+import nails from '../../assets/nails.jpg'
+import plum from '../../assets/plum.jpg'
+
+const ServiceList = [
+   {
+    name: "Chat with Us",
+    email:"specialistdoctor@gmail.com",
+    review:'7',
+    avator: doctor,
+    coord:{latitude:-28.678419, longitude: 153.387512}
+
+},{
+    name: "Do your nails with Us",
+    email:"nailspecialist@gmail.com",
+    review:'4',
+    avator: nails,
+    coord:{latitude:-28.658833, longitude: 153.385168}
+},{
+
+    name: "Plumber with Us",
+    email:"plumber@gmail.com",
+    review:'10',
+    avator: plum,
+    coord:{latitude:-28.652506, longitude: 153.408873}
+}, 
+
+]
+
+export default ServiceList;
